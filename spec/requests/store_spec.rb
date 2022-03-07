06536a5b0ee71d635ede5c7442de4390d04cd5a5
digital-diver <digital-diver@protonmail.com>
2022-03-07 14:45:36 +0100

    rm debugging code

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "Stores", type: :request do
   describe "GET /index" do
     it "returns http success" do
       get "/"
-      p response.body
 
       expect(response).to have_http_status(:success)
       assert_select ".side_nav a", minimum: 4
