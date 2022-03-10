61a4ec0617f5ad1f66278e2751ddb21e8e9bc7e1
digital-diver <digital-diver@protonmail.com>
2022-03-10 12:58:17 +0100

    add not nullable constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_142619) do
+ActiveRecord::Schema.define(version: 2022_03_10_115237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,14 +24,14 @@ ActiveRecord::Schema.define(version: 2022_03_09_142619) do
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "cart_id"
+    t.bigint "cart_id", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string "title", null: false
+    t.text "description", null: false
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", precision: 6, null: false
