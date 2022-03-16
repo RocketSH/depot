ea50eda2476e35466561fe60be85823592cac00d
digital-diver <digital-diver@protonmail.com>
2022-03-16 16:19:54 +0100

    add rescue_from on carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,15 @@
 class CartsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: %i[show edit update destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invaild_cart
+
   def show
     @cart = Cart.find(params[:id])
+  end
+
+  private
+  def invaild_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to root_url, notice: 'Sorry, you are querying an invalid cart'
   end
 end
