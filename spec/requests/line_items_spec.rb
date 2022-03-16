2d0459ba41c402cb351a281a5f433261d5d50ec4
digital-diver <digital-diver@protonmail.com>
2022-03-16 13:26:39 +0100

    test line_item product_id if == id from Product

--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -4,13 +4,16 @@ RSpec.describe 'LineItems', type: :request do
   describe '#create' do
     let(:factory_product) { create(:product) }
 
-    it 'create a line item' do
+    it 'create a new line item' do
       post line_items_path(product_id: factory_product)
 
       # 302 redirect: temporarily moved
       expect(response).to have_http_status(302)
       cart = Cart.last
       expect(response).to redirect_to(cart)
+
+      item = cart.line_items
+      expect(item.select(:product_id)) == (factory_product.id)
     end
   end
 end
