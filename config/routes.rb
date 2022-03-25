70e270693a9995e3e44692dd3093b98f23f9647a
digital-diver <digital-diver@protonmail.com>
2022-03-25 11:34:15 +0100

    rm routes nested of carts& line_items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'store#index'
   resources :products
 
-  resources :carts do
-    resources :line_items, only: %i[create update destroy]
-  end
+  resources :carts
+  resources :line_items, only: %i[create update destroy]
 end
