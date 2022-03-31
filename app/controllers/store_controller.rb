728ebe82680439f351f5572e6795d9894e2ea302
digital-diver <digital-diver@protonmail.com>
2022-03-31 15:12:07 +0200

    UI-move cart display to sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   before_action :increment_counter
+
   def index
     @products = Product.order(:title)
   end
