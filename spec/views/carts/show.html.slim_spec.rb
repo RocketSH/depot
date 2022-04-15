a7c4b68571d5e0d947485e0a924b3eb787241e5e
digital-diver <digital-diver@protonmail.com>
2022-04-15 15:00:18 +0200

    add logic to test if object has value in view spec

--- spec/views/carts/show.html.slim_spec.rb
+++ spec/views/carts/show.html.slim_spec.rb
@@ -5,10 +5,24 @@ RSpec.describe 'carts/show.html.slim', type: :view do
     @cart = FactoryBot.create(:cart)
     assign(:cart, @cart)
   end
+  subject { @cart.line_items }
 
   it 'renders a table of line items' do
+    expect(subject.count).to eq 2
+    cart_id = @cart[:id]
+    item_id_first = subject.first[:id]
+    item_qty_first = subject.first[:quantity]
+    item_id_second = subject.last[:id]
+    item_qty_second = subject.last[:quantity]
+    total_amount = subject.first[:price] + subject.last[:price]
+
     expect(render).to have_selector('h2')
+    expect(render).to match(/Your Current Cart ID:#{cart_id}/)
+    expect(render).to match(/#{item_id_first}/)
+    expect(render).to match(/#{item_id_second}/)
+    expect(render).to match(/#{item_qty_first}/)
+    expect(render).to match(/#{item_qty_second}/)
     expect(render).to have_selector('table')
-    expect(render).to match(/Total amount:/)
+    expect(render).to match(/#{total_amount}/)
   end
 end
