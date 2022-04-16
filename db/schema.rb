6b4d6186be4142d409b9960c4ab16fff27c23408
digital-diver <digital-diver@protonmail.com>
2022-04-16 09:52:39 +0200

    add orders table and associate with line_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_155319) do
+ActiveRecord::Schema.define(version: 2022_04_16_073713) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'carts', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'line_items', force: :cascade do |t|
-    t.bigint 'product_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'cart_id', null: false
-    t.integer 'quantity', default: 1
-    t.decimal 'price', precision: 8, scale: 2, null: false
-    t.index ['cart_id'], name: 'index_line_items_on_cart_id'
-    t.index ['product_id'], name: 'index_line_items_on_product_id'
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "cart_id"
+    t.integer "quantity", default: 1
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.bigint "order_id"
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'description', null: false
-    t.string 'image_url'
-    t.decimal 'price', precision: 8, scale: 2, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "email"
+    t.integer "pay_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'line_items', 'carts'
-  add_foreign_key 'line_items', 'products'
+  create_table "products", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.string "image_url"
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
 end
