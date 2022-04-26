d96a6df8fad5f89a2f2e25ebc0817b042d3c449d
digital-diver <digital-diver@protonmail.com>
2022-04-26 22:32:37 +0200

    install react js

---
+++ app/javascript/packs/PayTypeSelector/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function PayTypeSelector() {
+  return (
+    <div className="field">
+      <label htmlFor="order_pay_type">Pay type</label>
+      <select id="order_pay_type" name="order[pay_type]">
+        <option value="">Select a payment method</option>
+        <option value="Check">Check</option>
+        <option value="Credit card">Credit card</option>
+        <option value="Purchase order">Purchase order</option>
+      </selcet>
+    </div>
+  );
+}
+export default PayTypeSelector;
