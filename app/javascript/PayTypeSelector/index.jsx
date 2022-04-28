cd835d01d2aa391c4edf73392a09effa6d453d02
digital-diver <digital-diver@protonmail.com>
2022-04-28 16:46:04 +0200

    replace payType components with react hooks

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-// function PayTypeSelector() {
-function PayTypeSelector() {
+import NoPayType from "./NoPayType";
+import CreditCardPayType from "./CreditCardPayType";
+import CheckPayType from "./CheckPayType";
+import PurchaseOrderPayType from "./PurchaseOrderPayType";
+
+function PayTypeCustomComponent({ payType }) {
+  switch(payType) {
+    case "Credit card":
+      return <CreditCardPayType />
+    case "Check":
+      return <CheckPayType />
+    case "Purchase order":
+      return <PurchaseOrderPayType />
+    default: 
+      return <NoPayType />
+  }
+}
+
+export default function PayTypeSelector() {
+  const [payType, setPayType] = useState(null);
+
+  function onPayTypeSelectedHandler(event) {
+    setPayType(event.target.value);
+  }
+
   return (
-    <div className="field">
-      <label htmlFor="order_pay_type">Pay type</label>
-      <select id="order_pay_type" name="order[pay_type]">
-        <option value="">Select a payment method</option>
-        <option value="Check">Check</option>
-        <option value="Credit card">Credit card</option>
-        <option value="Purchase order">Purchase order</option>
-      </select>
+    <div>
+      <div className="field">
+        <label htmlFor="order_pay_type">Pay type</label>
+        <select
+          onChange={onPayTypeSelectedHandler}
+          id="order_pay_type"
+          name="order[pay_type]"
+        >
+          <option value="">Select a payment method</option>
+          <option value="Check">Check</option>
+          <option value="Credit card">Credit card</option>
+          <option value="Purchase order">Purchase order</option>
+        </select>
+      </div>
+      <PayTypeCustomComponent payType={payType} />
     </div>
   );
 }
-export default PayTypeSelector;
--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PayTypeSelector from 'PayTypeSelector'
+import React from "react";
+import ReactDOM from "react-dom";
+import PayTypeSelector from "PayTypeSelector";
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
   const element = document.getElementById("pay-type-component");
   ReactDOM.render(<PayTypeSelector />, element);
 });
