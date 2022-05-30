0104b2791d0d47f2885cd412f45266cd491b66b0
digital-diver <digital-diver@protonmail.com>
2022-05-30 14:47:38 +0200

    add action.mailer host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,12 +57,11 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "demo_production"
+  # config.active_job.queue_adapter  ix = "demo_production"
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://swiat-zabawek-staging.herokuapp.com/" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
